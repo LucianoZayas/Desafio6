afd6df1c53df0945e7280c18e3bdcc9a1c685cb1
LucianoZayas <luchozayas@hotmail.com>
2023-08-10 23:49:42 -0300

    eliminadas

--- Clase 13 - After 31 del 07/my-ecommerce/src/Database/firebaseConfig.js
+++
@@ -1,3 +0,0 @@
-export const realtime_database_url = 'https://ecommerce-43850-7d12e-default-rtdb.firebaseio.com/'
-
-export const api_key = 'AIzaSyB_R4X4bEIh0Iq1lKqnth-lChr8UKVgpiY'
--- Clase 17 - Workshop/my-ecommerce/src/Assets/Fonts/index.js
+++
@@ -1,4 +0,0 @@
-export const fonts = {
-    'Josefin': require('./Josefin_Sans/JosefinSans-Regular.ttf'),
-    'Ubuntu': require('./Ubuntu/Ubuntu-Regular.ttf')
-}
